9d468a4e37cd2b8b1e07a729a56a8d5f6747ce7d
MichaelBoateng <michaelboateng064@gmail.com>
2025-07-10 15:32:16 +0000

    Added static files

--- votingsystem/votingsystem/urls.py
+++ votingsystem/votingsystem/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from votingapp.views import index
 
 urlpatterns = [
@@ -24,3 +26,6 @@ urlpatterns = [
     path('', include('votingapp.urls')),
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
