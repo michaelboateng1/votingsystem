fb5cdd2cc6eb030346c313127b96feea0863e655
MichaelBoateng <michaelboateng064@gmail.com>
2025-07-10 14:59:54 +0000

    Prepared project for deplyment

--- votingsystem/votingsystem/settings.py
+++ votingsystem/votingsystem/settings.py
@@ -21,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$yvq_k^_er=w%u+%t!fsr04offorlf!v1cm(f_dv$^jj@*9o*)'
+# It is recommended to set the secret key in an environment variable.
+# If the environment variable is not set, a default key will be used (not for production).
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-$yvq_k^_er=w%u+%t!fsr04offorlf!v1cm(f_dv$^jj@*9o*)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG is False by default. Set the DEBUG environment variable to 'True' to enable it.
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS is configured via an environment variable.
+# It should be a comma-separated list of hosts.
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+
 
 
 # Application definition
@@ -78,10 +84,11 @@ WSGI_APPLICATION = 'votingsystem.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
@@ -121,6 +128,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
